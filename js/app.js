0934b83daa6622886fc02c5e5ab014b685eba216
Andy Woodward <andy.woodward1967@gmail.com>
2023-02-07 14:45:17 +0000

    Create a function to create the tasks that is called by the function that adds tasks contained in an array to the first column

--- js/app.js
+++ js/app.js
@@ -15,10 +15,39 @@ let tasks = [
       'The terms and conditions need updating as per the business meeting.',
   },
   {
-    id: 0,
-    title: '',
-    description: '',
+    id: 2,
+    title: 'Change banner picture',
+    description:
+      'Marketing has requested a new banner to be added to the website',
   },
 ]
 
-console.log(backlogTasks)
+function createTask(taskId, title, description) {
+  const taskCard = document.createElement('div')
+  const taskHeader = document.createElement('div')
+  const taskTitle = document.createElement('p')
+  const taskDescriptionContainer = document.createElement('div')
+  const taskDescription = document.createElement('p')
+  const deleteIcon = document.createElement('p')
+
+  taskCard.classList.add('task-container')
+  taskHeader.classList.add('task-header')
+  taskDescriptionContainer.classList.add('task-description-container')
+
+  taskTitle.textContent = title
+  taskDescription.textContent = description
+  deleteIcon.textContent = '☒'
+
+  taskHeader.append(taskTitle, deleteIcon)
+  taskDescriptionContainer.append(taskDescription)
+  taskCard.append(taskHeader, taskDescriptionContainer)
+  backlogTasks.append(taskCard)
+}
+
+function addTasks() {
+  tasks.forEach(task =>
+    createTask(task.id, task.title, task.description)
+  )
+}
+
+addTasks()
